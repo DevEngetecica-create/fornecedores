96b56f821dccf716a5e8c7ec0c28457e8051c870
Samuel Ferreira de Melo <samueltgq@gmail.com>
2024-07-03 14:26:40 -0300

    Commit: Criado os componentes Livewire para Fornecedor

---
+++ app/Livewire/Fornecedores/Show.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Fornecedores;
+
+use Livewire\Component;
+
+class Show extends Component
+{
+    public function render()
+    {
+        return view('livewire.fornecedores.show');
+    }
+}
